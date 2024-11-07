b78d91210ff2306d76a31639841b293dfee4a233
Fredrick Denga <dengafred@gmail.com>
2024-11-07 10:07:20 -0800

    implement products controller

---
+++ ProductApi/ProductApi.Presentation/Controllers/ProductsController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApi.Application.DTO;
+using ProductApi.Application.DTO.Conversions;
+using ProductApi.Application.Interfaces;
+using SharedLib.Responses;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ProductApi.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(IProduct productInterface) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetProducts()
+        {
+            //get all products from repo
+            var products = await productInterface.GetAllAsync();
+            if (!products.Any())
+            {
+                return NotFound("No products detected in the database");
+            }
+            //convert data from entity to DTO and return it
+            var list = ProductConversion.FromEntity(null!, products);
+            return list!.Any() ? Ok(list) : NotFound("No product found");
+        }
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ProductDTO>> GetProduct(int id)
+        {
+            //get single product from the repo
+            var product = await productInterface.FindByIDAsync(id);
+            if (product is null)
+            {
+                return NotFound("Product requested not found");
+            }
+            //convert from the entity to DTO and return
+            var _product = ProductConversion.FromEntity(product, null!);
+            return _product is not null ? Ok(_product) : NotFound("Product not found");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Response>> CreateProduct(ProductDTO product)
+        {
+            //check model state if all data annotations are passed
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            //convert to entity
+            var getEntity = ProductConversion.ToEntity(product);
+            var response = await productInterface.CreateAsync(getEntity);
+            return response.Flag is true ? Ok(response) : BadRequest(response);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Response>> UpdateProduct(ProductDTO product)
+        {
+            //check model state if all data annotations are passed
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            //convert to entity
+            var getEntity = ProductConversion.ToEntity(product);
+            var response = await productInterface.UpdateAsync(getEntity);
+            return response.Flag is true ? Ok(response) : BadRequest(response);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Response>> DeleteProduct(ProductDTO product)
+        {
+            //convert to entity
+            var getEntity = ProductConversion.ToEntity(product);
+            var response = await productInterface.DeleteAsync(getEntity);
+            return response.Flag is true ? Ok(response) : BadRequest(response);
+        }
+    }
+
+}
