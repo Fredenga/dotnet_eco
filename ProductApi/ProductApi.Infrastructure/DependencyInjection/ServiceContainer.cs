027121b35b45cd80769516f6a036110a81ca2515
Fredrick Denga <dengafred@gmail.com>
2024-11-07 08:00:53 -0800

    add dependency injection to infrastructure

---
+++ ProductApi/ProductApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApi.Application.Interfaces;
+using ProductApi.Infrastructure.Data;
+using ProductApi.Infrastructure.Repositories;
+using SharedLib.DependencyInjection;
+
+namespace ProductApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            //Add database connectivity
+            //Add Authentication scheme
+            SharedServiceContainer.AddSharedServices < ProductDbContext>(services, config, config["MySerilog:FindName"]!);
+            // Create dependency injection (Interface <-> Repo)
+            services.AddScoped<IProduct, ProductRepository>();
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            //register middleware:
+            //Global ex: external errors
+            //ListenToApiGateway
+            SharedServiceContainer.useSharedPolicies(app);
+            return app;
+        }
+    }
+}
